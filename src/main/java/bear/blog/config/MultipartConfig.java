28226b10b654f8a05f1c50e86b6fbb42e452288f
Jorge Morin <jmorin1@hawk.iit.edu>
2023-05-16 14:36:44 -0500

    Added new limit to file size submissions

---
+++ src/main/java/bear/blog/config/MultipartConfig.java
@@ -0,0 +1,22 @@
+package bear.blog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(124));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(124));
+        return factory.createMultipartConfig();
+    }
+}
+
